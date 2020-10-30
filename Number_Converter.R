16df6f98086f3495e25c6284a3e393195a531f9b
jdk1114 <73687783+jdk1114@users.noreply.github.com>
2020-10-30 07:52:44 +0000

    Add files via upload

--- Number_Converter.R
+++ Number_Converter.R
@@ -1,4 +1,12 @@
-input <- "The pump is 23 456,9 KGs deep underground"
+input <- NULL
+
+## Reading in text files
+input <- read.delim("C:/Users/justi/OneDrive/Documents/Job applications/UK CV/91/Examples.txt", header = FALSE, stringsAsFactors = FALSE)
+
+## Manually entering an input
+if (is.null(input)) {
+  input <- "The pump is 23 456,9 KGs deep underground"  
+}
 
 ## disabling scientific notation 
 options(scipen = 999)
@@ -11,122 +19,130 @@ tens = c(ten = 10, twenty = 20, thirty = 30, forty = 40, fifty = 50, sixty = 60,
 doubleDigits <- c(teens, tens)
 magnitudes <- c("thousand", "million", "billion", "trillion")
 
-## Extracting number from input and checking whether it is valid
-numberInput <- gsub(".*?([0-9]+).*", "\\1", input)
+## Convert input to a data.frame to ensure consistency in data type when either typing in an input or reading in multiple inputs from a text file
+input <- data.frame(input)
 
-## Check whether number is valid - first initialise output to NULL
-output <- NULL
-
-## 1. There should only be one continuous string of numbers
-digitStartLocations <- gregexpr("([0-9]+)", input)[[1]]
-
-if (length(digitStartLocations) > 1) {
-  output <- "number invalid"
-} 
-
-## 2. There should not be any characters directly before or after the number
-# To find end locations of digits need to reverse the order of the string
-reversedInput <- paste(rev(strsplit(input, "")[[1]]), collapse = "")
-digitEndLocations <- sort(nchar(input) - gregexpr("([0-9]+)", reversedInput)[[1]] + 1)
-if (digitStartLocations[1] != 1) {
-  if (strsplit(input, "")[[1]][digitStartLocations[1] - 1] != " ") {
+## Looping over each input value
+output <- sapply(1:nrow(input), function(i) {
+  ## Extracting number from input and checking whether it is valid
+  numberInput <- gsub(".*?([0-9]+).*", "\\1", input[i,])
+  
+  ## Check whether number is valid - first initialise output to NULL
+  output <- NULL
+  
+  ## 1. There should only be one continuous string of numbers
+  digitStartLocations <- gregexpr("([0-9]+)", input[i,])[[1]]
+  
+  if (length(digitStartLocations) > 1) {
     output <- "number invalid"
-  }
-}
-if (digitEndLocations[1] != nchar(input)) {
-  if (strsplit(input, "")[[1]][digitEndLocations[1] + 1] != " ") {
-    output <- "number invalid"
-  }
-}
-
-#####################
-## Operations if number is  valid
-if (!is.null(output)) {
-  ## Separating the digits into a vector
-  digitsSplit <- strsplit(numberInput, "")[[1]]
-  numberOfDigits <- length(digitsSplit)
+  } 
   
-  ## Functions
-  doubleDigitFn <- function(digits) {
-    ## If two digits are in the stored numbers then use these. Otherwise, use first digit and tens
-    ifelse(as.numeric(paste(digits, collapse = "")) %in% doubleDigits,
-           names(doubleDigits)[doubleDigits == as.numeric(paste(digits, collapse = ""))],
-           paste(names(tens)[tens == as.numeric(paste0(digits[1], 0))],
-                 ifelse(digits[2] != "0", names(singleDigits)[singleDigits == as.numeric(digits[2])], ""),
-                 sep = ifelse(!(digits[1] %in% "0"), "-", ""))
-    )
+  ## 2. There should not be any characters directly before or after the number
+  # To find end locations of digits need to reverse the order of the string
+  reversedInput <- paste(rev(strsplit(input[i,], "")[[1]]), collapse = "")
+  digitEndLocations <- sort(nchar(input[i,]) - gregexpr("([0-9]+)", reversedInput)[[1]] + 1)
+  if (digitStartLocations[1] != 1) {
+    if (strsplit(input[i,], "")[[1]][digitStartLocations[1] - 1] != " ") {
+      output <- "number invalid"
+    }
+  }
+  if (digitEndLocations[1] != nchar(input[i,])) {
+    if (strsplit(input[i,], "")[[1]][digitEndLocations[1] + 1] != " ") {
+      output <- "number invalid"
+    }
   }
   
-  hundredsFn <- function(digits) {
-    ## Calculate what the last two digits will output
-    lastDigits <- doubleDigitFn(digits[-1])
+  #####################
+  ## Operations if number is  valid
+  if (is.null(output)) {
+    ## Separating the digits into a vector
+    digitsSplit <- strsplit(numberInput, "")[[1]]
+    numberOfDigits <- length(digitsSplit)
     
-    paste0(ifelse(!(digits[1] %in% "0"),
-                  paste(names(singleDigits)[singleDigits == as.numeric(digits[1])], "hundred"),
-                  ""
-    ), 
-    paste0(ifelse(!(digits[1] %in% "0") & lastDigits != "", " and ", ""),ifelse(lastDigits != "", lastDigits, ""))
-    )
-  }
-  
-  magnitudesFn <- function(digits) {
+    ## Functions
+    doubleDigitFn <- function(digits) {
+      ## If two digits are in the stored numbers then use these. Otherwise, use first digit and tens
+      ifelse(as.numeric(paste(digits, collapse = "")) %in% doubleDigits,
+             names(doubleDigits)[doubleDigits == as.numeric(paste(digits, collapse = ""))],
+             paste(names(tens)[tens == as.numeric(paste0(digits[1], 0))],
+                   ifelse(digits[2] != "0", names(singleDigits)[singleDigits == as.numeric(digits[2])], ""),
+                   sep = ifelse(!(digits[1] %in% "0"), "-", ""))
+      )
+    }
     
-    ## Calculate the modular to determine whether it is a single digit, a double digit or a triple digit magnitude prefix
-    prefixMagnitude <- (length(digits) - 1) %% 3 + 1
+    hundredsFn <- function(digits) {
+      ## Calculate what the last two digits will output
+      lastDigits <- doubleDigitFn(digits[-1])
+      
+      paste0(ifelse(!(digits[1] %in% "0"),
+                    paste(names(singleDigits)[singleDigits == as.numeric(digits[1])], "hundred"),
+                    ""
+      ), 
+      paste0(ifelse(!(digits[1] %in% "0") & lastDigits != "", " and ", ""),ifelse(lastDigits != "", lastDigits, ""))
+      )
+    }
     
-    if (!(all(digits[1:prefixMagnitude] %in% "0"))) {
-      ## Find the first values that are non-zero
-      digits <- strsplit(as.character(as.numeric(paste(digits, collapse = ""))), "")[[1]]
+    magnitudesFn <- function(digits) {
       
       ## Calculate the modular to determine whether it is a single digit, a double digit or a triple digit magnitude prefix
       prefixMagnitude <- (length(digits) - 1) %% 3 + 1
       
-      ## Choose prefix from list depending on the number of digits in the number, e.g. 4, 5 or 6 digits is in the thousands, 7, 8 or 9 digits is in the millions, ...
-      prefix <- magnitudes[floor((length(digits) - 1) / 3)]
-      
-      output <- paste(switch(as.character(prefixMagnitude),
-                             "1" = names(singleDigits)[singleDigits == as.numeric(digits[1])], 
-                             "2" = doubleDigitFn(digits[1:2]),
-                             "3" = hundredsFn(digits[1:3])),
-                      prefix
-      )
+      if (!(all(digits[1:prefixMagnitude] %in% "0"))) {
+        ## Find the first values that are non-zero
+        digits <- strsplit(as.character(as.numeric(paste(digits, collapse = ""))), "")[[1]]
+        
+        ## Calculate the modular to determine whether it is a single digit, a double digit or a triple digit magnitude prefix
+        prefixMagnitude <- (length(digits) - 1) %% 3 + 1
+        
+        ## Choose prefix from list depending on the number of digits in the number, e.g. 4, 5 or 6 digits is in the thousands, 7, 8 or 9 digits is in the millions, ...
+        prefix <- magnitudes[floor((length(digits) - 1) / 3)]
+        
+        output <- paste(switch(as.character(prefixMagnitude),
+                               "1" = names(singleDigits)[singleDigits == as.numeric(digits[1])], 
+                               "2" = doubleDigitFn(digits[1:2]),
+                               "3" = hundredsFn(digits[1:3])),
+                        prefix
+        )
+      } else {
+        output <- ""
+      }
+    }
+    
+    if (numberOfDigits == 1) {
+      output <- names(singleDigits)[singleDigits == as.numeric(input)]
+    } else if (numberOfDigits == 2) {
+      output <- doubleDigitFn(digitsSplit)
+    } else if (numberOfDigits == 3) {
+      output <- hundredsFn(digitsSplit)
     } else {
-      output <- ""
+      ## Calculate the modular to determine whether it is a single digit, a double digit or a triple digit magnitude prefix
+      prefixMagnitude <- (length(digitsSplit) - 1) %% 3 + 1
+      
+      output <- magnitudesFn(digitsSplit)
+      
+      ## Creating a while loop which will run as long as there are still more than 3 digits left to assign wording to
+      updatedDigitsSplit <- digitsSplit[-(1:prefixMagnitude)]
+      
+      while (length(updatedDigitsSplit) > 3) {
+        ## Create new variable with next output stored
+        nextOutput <- magnitudesFn(updatedDigitsSplit)
+        output <- paste(output, nextOutput, sep = ifelse(nextOutput != "", ", ", ""))
+        
+        prefixMagnitude <- (length(updatedDigitsSplit) - 1) %% 3 + 1
+        updatedDigitsSplit <- updatedDigitsSplit[-(1:prefixMagnitude)]
+      }
+      ## Create new variable with next output stored
+      nextOutput <- hundredsFn(updatedDigitsSplit)
+      output <- paste(output, nextOutput, sep = ifelse(nextOutput != "", 
+                                                       ifelse(updatedDigitsSplit[1] %in% "0", 
+                                                              " and ",
+                                                              ", "
+                                                       ), "")
+      )
     }
   }
   
-  if (numberOfDigits == 1) {
-    output <- names(singleDigits)[singleDigits == as.numeric(input)]
-  } else if (numberOfDigits == 2) {
-    output <- doubleDigitFn(digitsSplit)
-  } else if (numberOfDigits == 3) {
-    output <- hundredsFn(digitsSplit)
-  } else {
-    ## Calculate the modular to determine whether it is a single digit, a double digit or a triple digit magnitude prefix
-    prefixMagnitude <- (length(digitsSplit) - 1) %% 3 + 1
-    
-    output <- magnitudesFn(digitsSplit)
-    
-    ## Creating a while loop which will run as long as there are still more than 3 digits left to assign wording to
-    updatedDigitsSplit <- digitsSplit[-(1:prefixMagnitude)]
-    
-    while (length(updatedDigitsSplit) > 3) {
-      ## Create new variable with next output stored
-      nextOutput <- magnitudesFn(updatedDigitsSplit)
-      output <- paste(output, nextOutput, sep = ifelse(nextOutput != "", ", ", ""))
-      
-      prefixMagnitude <- (length(updatedDigitsSplit) - 1) %% 3 + 1
-      updatedDigitsSplit <- updatedDigitsSplit[-(1:prefixMagnitude)]
-    }
-    ## Create new variable with next output stored
-    nextOutput <- hundredsFn(updatedDigitsSplit)
-    output <- paste(output, nextOutput, sep = ifelse(nextOutput != "", 
-                                                     ifelse(updatedDigitsSplit[1] %in% "0", 
-                                                            " and ",
-                                                            ", "
-                                                     ), "")
-    )
-  }
-}
+  return(output)
+})
 
 output
