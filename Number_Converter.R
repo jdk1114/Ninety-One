c14cc9784ad3ca5c054bcba6238ba529090b7a69
jdk1114 <73687783+jdk1114@users.noreply.github.com>
2020-11-01 12:11:06 +0000

    Add files via upload

--- Number_Converter.R
+++ Number_Converter.R
@@ -1,12 +1,8 @@
-input <- NULL
-
 ## Reading in text files
-input <- read.delim("C:/Users/justi/OneDrive/Documents/Job applications/UK CV/91/Examples.txt", header = FALSE, stringsAsFactors = FALSE)
+input <- read.delim("https://raw.github.com/jdk1114/Ninety-One/main/Examples.txt", header = FALSE, stringsAsFactors = FALSE)
 
 ## Manually entering an input
-if (is.null(input)) {
-  input <- "The pump is 23 456,9 KGs deep underground"  
-}
+input <- "The pump is 1000000 KGs deep underground"
 
 ## disabling scientific notation 
 options(scipen = 999)
@@ -20,18 +16,18 @@ doubleDigits <- c(teens, tens)
 magnitudes <- c("thousand", "million", "billion", "trillion")
 
 ## Convert input to a data.frame to ensure consistency in data type when either typing in an input or reading in multiple inputs from a text file
-input <- data.frame(input)
+input <- data.frame(input, stringsAsFactors = FALSE)
 
-## Looping over each input value
-output <- sapply(1:nrow(input), function(i) {
+## Function that converts numbers to words
+numbersToWords <- function(textInput) {
   ## Extracting number from input and checking whether it is valid
-  numberInput <- gsub(".*?([0-9]+).*", "\\1", input[i,])
+  numberInput <- gsub(".*?([0-9]+).*", "\\1", textInput)
   
   ## Check whether number is valid - first initialise output to NULL
   output <- NULL
   
   ## 1. There should only be one continuous string of numbers
-  digitStartLocations <- gregexpr("([0-9]+)", input[i,])[[1]]
+  digitStartLocations <- gregexpr("([0-9]+)", textInput)[[1]]
   
   if (length(digitStartLocations) > 1) {
     output <- "number invalid"
@@ -39,15 +35,15 @@ output <- sapply(1:nrow(input), function(i) {
   
   ## 2. There should not be any characters directly before or after the number
   # To find end locations of digits need to reverse the order of the string
-  reversedInput <- paste(rev(strsplit(input[i,], "")[[1]]), collapse = "")
-  digitEndLocations <- sort(nchar(input[i,]) - gregexpr("([0-9]+)", reversedInput)[[1]] + 1)
+  reversedInput <- paste(rev(strsplit(textInput, "")[[1]]), collapse = "")
+  digitEndLocations <- sort(nchar(textInput) - gregexpr("([0-9]+)", reversedInput)[[1]] + 1)
   if (digitStartLocations[1] != 1) {
-    if (strsplit(input[i,], "")[[1]][digitStartLocations[1] - 1] != " ") {
+    if (strsplit(textInput, "")[[1]][digitStartLocations[1] - 1] != " ") {
       output <- "number invalid"
     }
   }
-  if (digitEndLocations[1] != nchar(input[i,])) {
-    if (strsplit(input[i,], "")[[1]][digitEndLocations[1] + 1] != " ") {
+  if (digitEndLocations[1] != nchar(textInput)) {
+    if (strsplit(textInput, "")[[1]][digitEndLocations[1] + 1] != " ") {
       output <- "number invalid"
     }
   }
@@ -55,17 +51,29 @@ output <- sapply(1:nrow(input), function(i) {
   #####################
   ## Operations if number is  valid
   if (is.null(output)) {
-    ## Separating the digits into a vector
+    ## Separating the digits into a vector to 
     digitsSplit <- strsplit(numberInput, "")[[1]]
+    
+    # Determine length of number to call correct functions in operations
     numberOfDigits <- length(digitsSplit)
     
-    ## Functions
+    ## Remove first digit until it is not zero if length is greater than one
+    while(digitsSplit[1] %in% "0" & numberOfDigits > 1) {
+      digitsSplit <- digitsSplit[-1]
+      numberOfDigits <- length(digitsSplit)
+    }
+    
+    ## Functions that return numbers as words
+    singleDigitsFn <- function(digits) {
+      names(singleDigits)[singleDigits == as.numeric(digits)]
+    }
+    
     doubleDigitFn <- function(digits) {
       ## If two digits are in the stored numbers then use these. Otherwise, use first digit and tens
       ifelse(as.numeric(paste(digits, collapse = "")) %in% doubleDigits,
              names(doubleDigits)[doubleDigits == as.numeric(paste(digits, collapse = ""))],
              paste(names(tens)[tens == as.numeric(paste0(digits[1], 0))],
-                   ifelse(digits[2] != "0", names(singleDigits)[singleDigits == as.numeric(digits[2])], ""),
+                   ifelse(digits[2] != "0", singleDigitsFn(digits[2]), ""),
                    sep = ifelse(!(digits[1] %in% "0"), "-", ""))
       )
     }
@@ -74,19 +82,23 @@ output <- sapply(1:nrow(input), function(i) {
       ## Calculate what the last two digits will output
       lastDigits <- doubleDigitFn(digits[-1])
       
+      ## Return the number to prefix the word 'hundred' with
       paste0(ifelse(!(digits[1] %in% "0"),
-                    paste(names(singleDigits)[singleDigits == as.numeric(digits[1])], "hundred"),
+                    paste(singleDigitsFn(digits[1]), "hundred"),
                     ""
       ), 
-      paste0(ifelse(!(digits[1] %in% "0") & lastDigits != "", " and ", ""),ifelse(lastDigits != "", lastDigits, ""))
+      ## If the last two digits are both not zero then include 'and' to combine the first digit and the last two digits
+      ifelse(lastDigits != "", paste0(" and ", lastDigits), "")
       )
     }
     
     magnitudesFn <- function(digits) {
+      ## Used for any numbers greater than or equal to 1000.
       
       ## Calculate the modular to determine whether it is a single digit, a double digit or a triple digit magnitude prefix
       prefixMagnitude <- (length(digits) - 1) %% 3 + 1
       
+      ## If all the digits sent to the function are zero then return nothing
       if (!(all(digits[1:prefixMagnitude] %in% "0"))) {
         ## Find the first values that are non-zero
         digits <- strsplit(as.character(as.numeric(paste(digits, collapse = ""))), "")[[1]]
@@ -98,7 +110,7 @@ output <- sapply(1:nrow(input), function(i) {
         prefix <- magnitudes[floor((length(digits) - 1) / 3)]
         
         output <- paste(switch(as.character(prefixMagnitude),
-                               "1" = names(singleDigits)[singleDigits == as.numeric(digits[1])], 
+                               "1" = singleDigitsFn(digits[1]), 
                                "2" = doubleDigitFn(digits[1:2]),
                                "3" = hundredsFn(digits[1:3])),
                         prefix
@@ -108,8 +120,9 @@ output <- sapply(1:nrow(input), function(i) {
       }
     }
     
+    ## Call the appropriate function based on the length of the digits
     if (numberOfDigits == 1) {
-      output <- names(singleDigits)[singleDigits == as.numeric(input)]
+      output <- singleDigitsFn(digitsSplit) 
     } else if (numberOfDigits == 2) {
       output <- doubleDigitFn(digitsSplit)
     } else if (numberOfDigits == 3) {
@@ -143,6 +156,9 @@ output <- sapply(1:nrow(input), function(i) {
   }
   
   return(output)
-})
+}
+
+## Looping over each input value
+output <- sapply(1:nrow(input), function(i) numbersToWords(input[i,]))
 
 output
